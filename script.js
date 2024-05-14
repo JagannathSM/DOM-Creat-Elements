e3b8eb7fcda5b20adb602b3ba1f76a44545646e0
JagannathSM <167777809+JagannathSM@users.noreply.github.com>
2024-05-14 19:32:05 +0530

    DOM-Element create

--- script.js
+++ script.js
@@ -14,6 +14,22 @@ function input_creat(inputtag,attri1,attri1val,attri2,attri2val){
     out.setAttribute(attri2,attri2val);
     return out;
 }
+function button_creat(buttontag,attri1,attri1val,attri2,attri2val,content){
+    var out = document.createElement(buttontag);
+    out.setAttribute(attri1,attri1val);
+    out.setAttribute(attri2,attri2val);
+    out.innerText = content;
+    return out;
+}
+function getvalues(){
+    var first=document.getElementById("first").value;
+    var middle=document.getElementById("middle").value;
+    var last=document.getElementById("last").value;
+    var email=document.getElementById("email").value;
+    var address=document.getElementById("add").value;
+
+    console.log(`FirstName:${first}, MiddleName:${middle}, LastName:${last}, Email:${email}, Address:${address}`);
+}
 
 
 document.body.append(label_creat("label","for","first","FirstName:"));
@@ -37,4 +53,4 @@ document.body.append(break_creat());
 document.body.append(input_creat("input","type","text","id","add"));
 document.body.append(break_creat());
 document.body.append(break_creat());
-document.body.append(label_creat("button","type","button","Submit"));
+document.body.append(button_creat("input","type","button","onclick","getvalues()","Submit"));
